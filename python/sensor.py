c70d03c1e0becb4d152ecd66510b8d4796953417
esmitt <esmitt.ramirez@gmail.com>
2020-12-17 21:32:22 +0100

    cleaned code to test it

--- python/sensor.py
+++ python/sensor.py
@@ -3,8 +3,7 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 from pyquaternion import Quaternion
 import pandas as pd
-import typing
-
+import pickle
 
 # Q is a Nx4 numpy matrix and contains the quaternions to average in the rows.
 # The quaternions are arranged as (w,x,y,z), with w being the scalar
@@ -50,83 +49,30 @@ def remove_outliers(list_values: list, min_quantile: float = 0.1, max_quantile: 
 
     return return_list
 
-
-# def get_axis(list_quaternion: list) -> typing.Tuple[np.real, np.array]:
-#     list_red = remove_outliers(list_quaternion)
-#     q = average_quaternions(np.asarray(list_red, dtype=np.float32))
-#     rx, ry, rz = compute_sensor_axis(q[0], q[1], q[2], q[3])
-#     return q, rz
-
-
 def measure_error(v1: np.ndarray, v2: np.ndarray) -> float:
     return mean_squared_error(v1, v2)
-
 
 def quadratic_error(v1: np.ndarray, v2: np.ndarray) -> float:
     return (sqrt(v1[0]*v2[0]) + (v1[1]*v2[1]) + (v1[2]*v2[2]))
 
+def get_rotmat(angle: float, ux: float, uy: float, uz: float) -> np.array:
+     ry = np.array([ux * uy * (1 - cos(angle)) - uz * sin(angle),
+                      cos(angle) + uy * uy * (1 - cos(angle)),
+                      uz * uy * (1 - cos(angle)) + ux * sin(angle)])
+     rx = np.array([cos(angle) + ux*ux*(1 - cos(angle)),
+                    uy*ux*(1 - cos(angle)) + uz*sin(angle),
+                    uz*ux*(1 - cos(angle)) - uy*sin(angle)])
+     rz = np.array([ux*uz*(1 - cos(angle)) + uy*sin(angle),
+                    uy*uz*(1 - cos(angle)) - ux*sin(angle),
+                    cos(angle) + uz*uz*(1 - cos(angle))])
 
-# def compute_sensor_axis3(w: float, x: float, y: float, z: float) -> np.ndarray:
-#     cos_a = w
-#     angle = 2.0 * acos(w)  # returns angle in radians
-#     norm = sqrt(x * x + y * y + z * z)
-#     if norm == 0:
-#         return np.zeros(3)
-#     # ux = -x / norm
-#     ux = x / norm
-#     uy = y / norm
-#     uz = z / norm
-#     sin_a = sqrt(1.0 - cos_a * cos_a)
-#     if abs(sin_a) < 0.0005:
-#         sin_a = 1
-#     return angle, np.array([ux / sin_a, uy / sin_a, uz / sin_a])
+     return np.array([rx, ry, rz])
 
-#
-# def compute_sensor_axis(w: float, x: float, y: float, z: float) -> np.ndarray:
-#     cos_a = w
-#     angle = 2.0 * acos(w)  # returns angle in radians
-#     norm = sqrt(x * x + y * y + z * z)
-#     if norm == 0:
-#         return np.zeros(3)
-#     # ux = -x / norm
-#     ux = x / norm
-#     uy = y / norm
-#     uz = z / norm
-#     sin_a = sqrt(1.0 - cos_a * cos_a)
-#     if abs(sin_a) < 0.0005:
-#         sin_a = 1
-#     return np.array([ux / sin_a, uy / sin_a, uz / sin_a])
-
-
-# def compute_sensor_axis(w: float, x: float, y: float, z: float) -> np.ndarray:
-#     angle = 2.0 * acos(w)  # returns angle in radians
-#     norm = sqrt(x * x + y * y + z * z)
-#     if norm == 0:
-#         return np.zeros(3)
-#     # ux = -x / norm
-#     ux = x / norm
-#     uy = y / norm
-#     uz = z / norm
-#     ry = np.array([ux * uy * (1 - cos(angle)) - uz * sin(angle),
-#                      cos(angle) + uy * uy * (1 - cos(angle)),
-#                      uz * uy * (1 - cos(angle)) + ux * sin(angle)])
-#     rx = np.array([cos(angle) + ux*ux*(1 - cos(angle)),
-#                    uy*ux*(1 - cos(angle)) + uz*sin(angle),
-#                    uz*ux*(1 - cos(angle)) - uy*sin(angle)])
-#     rz = np.array([ux*uz*(1 - cos(angle)) + uy*sin(angle),
-#                    uy*uz*(1 - cos(angle)) - ux*sin(angle),
-#                    cos(angle) + uz*uz*(1 - cos(angle))])
-#     rx[0] = -rx[0]
-#     ry[0] = -ry[0]
-#     rz[0] = -rz[0]
-#
-#     return rx, ry, rz
-
-def quaternion2rotmatrix_sensor(w: float, x: float, y: float, z: float) -> np.ndarray:
+def quat_to_rotmat_sensor(w: float, x: float, y: float, z: float) -> np.ndarray:
     angle = 2.0 * acos(w)  # returns angle in radians
     norm = sqrt(x * x + y * y + z * z)
     if norm == 0:
-        return np.array([np.zeros(3),np.zeros(3), np.zeros(3)])
+        return np.array([np.zeros(3), np.zeros(3), np.zeros(3)])
     # ux = -x / norm
     ux = x / norm
     uy = y / norm
@@ -145,19 +91,18 @@ def quaternion2rotmatrix_sensor(w: float, x: float, y: float, z: float) -> np.nd
 
     return np.transpose(np.array([ax, ay, az]))
 
-
 # return the 3 columns
-def compute_axis(w: float, x: float, y: float, z: float):
+def compute_axis(w: float, x: float, y: float, z: float) -> tuple:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    R = quaternion2rotmatrix_sensor(w, -x, z, y)  # columns
+    R = quat_to_rotmat_sensor(w, -x, z, y)  # columns
     #RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
     #RT = np.matmul(np.linalg.inv(T), R)
     RT = np.matmul(T, R)
     return RT[:, 0], RT[:, 1], RT[:, 2]
 
-def quaternion2rotmatrix_world1(w: float, x: float, y: float, z: float):
+def quaternion2rotmatrix_world1(w: float, x: float, y: float, z: float) -> np.ndarray:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    R = quaternion2rotmatrix_sensor(w, -x, z, y)  # columns
+    R = quat_to_rotmat_sensor(w, -x, z, y)  # columns
     #RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
     #RT = np.matmul(np.linalg.inv(T), R)
     RT = np.matmul(T, R) # original
@@ -165,9 +110,9 @@ def quaternion2rotmatrix_world1(w: float, x: float, y: float, z: float):
     #RT= np.matmul(np.linalg.inv(T), np.matmul(R, T))
     return RT
 
-def quaternion2rotmatrix_world2(w: float, x: float, y: float, z: float):
+def quaternion2rotmatrix_world2(w: float, x: float, y: float, z: float) -> np.ndarray:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    R = quaternion2rotmatrix_sensor(w, -x, z, y)  # columns
+    R = quat_to_rotmat_sensor(w, -x, z, y)  # columns
     #RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
     #RT = np.matmul(np.linalg.inv(T), R)
     # RT = np.matmul(T, R) # original
@@ -175,35 +120,23 @@ def quaternion2rotmatrix_world2(w: float, x: float, y: float, z: float):
     #RT= np.matmul(np.linalg.inv(T), np.matmul(R, T))
     return RT
 
-def quaternion2rotmatrix_world3(w: float, x: float, y: float, z: float):
+def quaternion2rotmatrix_world3(w: float, x: float, y: float, z: float) -> np.ndarray:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    R = quaternion2rotmatrix_sensor(w, -x, z, y)  # columns
+    R = quat_to_rotmat_sensor(w, -x, z, y)  # columns
     #RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
     #RT = np.matmul(np.linalg.inv(T), R)
     # RT = np.matmul(T, R) # original
     #RT = np.matmul(R, T)
-    RT= np.matmul(np.linalg.inv(T), np.matmul(R, T))
+    RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
     return RT
 
-def quaternion2rotmatrix_world(w: float, x: float, y: float, z: float):
+def quat_to_rotmat_world(q: list) -> np.ndarray:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
-    R = quaternion2rotmatrix_sensor(w, -x, z, y)  # columns
-    #RT = np.matmul(np.linalg.inv(T), np.matmul(R, T))
-    #RT = np.matmul(np.linalg.inv(T), R)
-    RT = np.matmul(T, R) # original
-    #RT = np.matmul(R, T)
-    #RT= np.matmul(np.linalg.inv(T), np.matmul(R, T))
+    R = quat_to_rotmat_sensor(q[0], -q[1], q[3], q[2])  # columns
+    RT = np.matmul(T, R)  # originaq
     return RT
 
-# def compute_transformed_axis(w: float, x: float, y: float, z: float, ax, ay, az):
-#     Rq = quaternion2rotmatrix(w, x, y, z)
-#     Tax = np.matmul(Rq, ax)
-#     Tay = np.matmul(Rq, ay)
-#     Taz = np.matmul(Rq, az)
-#     return Tax, Tay, Taz
-
-
-def sensor2world(v: np.array):
+def sensor2world(v: np.array) -> np.array:
     T = np.array([[-1, 0, 0], [0, 0, 1], [0, 1, 0]])
     return np.matmul(T, v)
 
@@ -228,65 +161,30 @@ def compute_axis_theoretical(theta: float, phi: float, ax, ay, az) -> np.ndarray
 
     return Tax, Tay, Taz
 
-#global Rq0
-def calibrate_angle0_world(q0: tuple, qlist: list) -> float:
-    Rq0 = quaternion2rotmatrix_world(q0[0], q0[1], q0[2], q0[3])
+
+def rotation_matrix_to_ypr(Rt)-> tuple:
+    yaw = degrees(atan2(Rt[1, 0], Rt[0, 0]))
+    roll = degrees(atan2(Rt[2, 1], Rt[2, 2]))
+    pitch = degrees(atan2(-Rt[2, 0], sqrt(pow(Rt[0, 0], 2) + pow(Rt[1, 0], 2))))
+    return yaw,pitch,roll
+
+def calibrate_angle0_world(q0: tuple, qlist: list) -> list:
+    #Rq0 = quat_to_rotmat_world(q0[0], q0[1], q0[2], q0[3])
+    Rq0 = quat_to_rotmat_world(q0)
     list_yaw = list()
 
     for q in qlist:
-        Rq = quaternion2rotmatrix_world(q[0], q[1], q[2], q[3])
-        Rt = np.matmul(Rq, np.linalg.inv(Rq0))
+        #Rq = quat_to_rotmat_world(q[0], q[1], q[2], q[3])
+        Rq = quat_to_rotmat_world(q)
+        #Rt = np.matmul(Rq, np.linalg.inv(Rq0))
+        Rt = np.matmul(np.linalg.inv(Rq0), Rq)
         yaw = degrees(atan2(Rt[1, 0], Rt[0, 0]))
         roll = degrees(atan2(Rt[2, 1], Rt[2, 2]))
         pitch = degrees(atan2(-Rt[2, 0], sqrt(pow(Rt[0, 0], 2) + pow(Rt[1, 0], 2))))
         list_yaw.append({"yaw":f"{yaw:.3f}", "pitch":f"{pitch:.3f}", "roll":f"{roll:.3f}"})
     return list_yaw
 
-
-# def calibrate_angle0_sensor(q0: tuple, qlist: list) -> float:
-#     Rq0 = quaternion2rotmatrix_sensor(q0[0], q0[1], q0[2], q0[3])
-#     #Rq0 = quaternion2rotmatrix_world(q0[0], q0[1], q0[2], q0[3])
-#     list_yaw = list()
-#
-#     for q in qlist:
-#         Rq = quaternion2rotmatrix_world(q[0], q[1], q[2], q[3])
-#         Rt = np.matmul(Rq, np.linalg.inv(Rq0))
-#         yaw = degrees(atan2(Rt[1, 0], Rt[0, 0]))
-#         roll = degrees(atan2(Rt[2, 1], Rt[2, 2]))
-#         pitch = degrees(atan2(-Rt[2, 0], sqrt(pow(Rt[0, 0], 2) + pow(Rt[1, 0], 2))))
-#         list_yaw.append({"yaw":f"{yaw:.3f}", "pitch":f"{pitch:.3f}", "roll":f"{roll:.3f}"})
-#     return list_yaw
-
-# def compute_sensor_theoretical(theta: float, phi: float, sensor_axis: np.ndarray) -> np.ndarray:
-#     """
-#     :param theta is the latitude
-#     :param phi is the longitude
-#     :param sensor_axis
-#     :return numpy array of theoretical sensor for the expected rotations
-#     """
-#
-#     def origin_angles(v: np.ndarray) -> float:
-#         theta_rad = atan2(v[1], v[0])
-#         theta_deg = (theta_rad / np.pi * 180)
-#         theta_deg = theta_deg  # + ((0 if theta_rad > 0 else 360) % 360)
-#         return theta_deg
-#
-#     theta_zero = origin_angles(sensor_axis)
-#     theta = theta + theta_zero
-#     # phi = 90 - phi
-#
-#     # yaw
-#     rz = np.array([[np.cos(radians(theta)), -np.sin(radians(theta)), 0],
-#                    [np.sin(radians(theta)), np.cos(radians(theta)), 0],
-#                    [0, 0, 1]])
-#     # pitch;
-#     ry = np.array([[np.cos(radians(phi)), 0, np.sin(radians(phi))],
-#                    [0, 1, 0],
-#                    [-np.sin(radians(phi)), 0, np.cos(radians(phi))]])
-#
-#     return np.matmul(rz, np.matmul(ry, sensor_axis))
-
-def fun(list_q, quaternion2rotmatrix):
+def fun(list_q: list, quaternion2rotmatrix) -> list:
     list_angles = list()
     for q in list_q:
         Rt = quaternion2rotmatrix(q[0], q[1], q[2], q[3])
@@ -296,52 +194,12 @@ def fun(list_q, quaternion2rotmatrix):
         list_angles.append({"yaw":f"{yaw:.3f}", "pitch":f"{pitch:.3f}", "roll":f"{roll:.3f}"})
     return list_angles
 
-# angles_world2 = fun(list_q, quaternion2rotmatrix_world)
-# angles_sensor2 = fun(list_q, quaternion2rotmatrix_sensor)
-# [print(a) for a in angles_sensor]
-#0.88,
-import pickle
-def load_quaternions(filename="quaternions.q"):
+
+def load_quaternions(filename: str = "quaternions.q") -> list:
     open_file = open(filename, "rb")
     loaded_list = pickle.load(open_file)
     open_file.close()
     return loaded_list
-#
-# list_q = load_quaternions()
-# q_world1 = fun(list_q, quaternion2rotmatrix_world1)
-# for q in q_world1:
-#     print(q)
-# print()
-# q_world2 = fun(list_q, quaternion2rotmatrix_world2)
-# for q in q_world2:
-#     print(q)
-# print()
-# q_world3 = fun(list_q, quaternion2rotmatrix_world3)
-# for q in q_world3:
-#     print(q)
-#
-# phi = 20
-# theta = 0
-#
-# def funRz(theta):
-#     R=np.array([[np.cos(radians(theta)), -np.sin(radians(theta)), 0],
-#                    [np.sin(radians(theta)), np.cos(radians(theta)), 0],
-#                    [0, 0, 1]])
-#     return R
-# # pitch;
-# def funRy(phi):
-#     R = np.array([[np.cos(radians(phi)), 0, np.sin(radians(phi))],
-#                [0, 1, 0],
-#                [-np.sin(radians(phi)), 0, np.cos(radians(phi))]])
-#     return R
-#
-# Rt = np.matmul(Rz, Ry)
-#
-# Rini = funRz(20)
-# Rz = funRz(0)
-# Ry = funRy(45)
-#
-# Rt = np.matmul(Ry, Rini)
 
 if __name__ == "__main__":
     ll = [[0.1, 0.2, 0.3, 0.4], [0.3, 0.4, 0.5, 0.6], [0.1, 0.2, 0.3, 0.4], [0.3, 0.4, 0.5, 0.6]]
--- python/test_sensor.py
+++ python/test_sensor.py
@@ -1,7 +1,7 @@
 from sensor import *
 import pickle
 
-def load_quaternions(filename="quaternions.q"):
+def load_quaternions(filename: str = "quaternions.q") -> list:
     open_file = open(filename, "rb")
     loaded_list = pickle.load(open_file)
     open_file.close()
@@ -18,10 +18,23 @@ def print_yaws(initial_q, ylist: list):
         print(f"{index+1}: {ylist[index]}")
 
 def rot_axis_angle_to_ypr(degrees: float, v: list, source: np.ndarray) -> tuple:
+    """
+    Function to rotate `source` an amount of `degrees` around the vector `v `
+    :param degrees: a value in degrees
+    :param v: axis vector where to rotate
+    :param source: vector to be rotated
+    :return: a tuple value representing yaw, pitch, roll
+    """
     rotation_matrix = get_rotmat(radians(degrees), v[0], v[1], v[2])
     return rotation_matrix_to_ypr(np.matmul(rotation_matrix, source))
 
 def error_ypr(rotmat: np.ndarray, ypr_theoretical: tuple) -> tuple:
+    """
+    Function to return the absolute error between two yaw, pitch, roll values
+    :param rotmat: rotation matrix where a yaw, pitch, roll is computed.
+    :param ypr_theoretical: theoretical yaw, pitch, roll to compare
+    :return:
+    """
     ypr = rotation_matrix_to_ypr(rotmat)
     error = np.abs(np.subtract(ypr , ypr_theoretical))
     return error
@@ -34,8 +47,6 @@ if __name__ == '__main__':
     rot_mat = [quat_to_rotmat_world(q) for q in quaternions]
     angles = [0, 20, 40, 60 , 80]
 
-    get_rotmat(radians(degrees), v[0], v[1], v[2])
-
     i = 1
     theoretical_rot = rot_axis_angle_to_ypr(angles[i], [1, 0, 0], rot_mat[i])
     print(error_ypr(rot_mat[i], theoretical_rot))
@@ -43,72 +54,3 @@ if __name__ == '__main__':
     i = 2
     theoretical_rot = rot_axis_angle_to_ypr(angles[i], rot_mat[0][:, 1], rot_mat[i])
     print(error_ypr(rot_mat[i], theoretical_rot))
-
-    # for index in range(1, len(angles)):
-    #     rot_theoretical = rot_axis_angle_to_ypr(-angles[index], [0, 0, 1], rot_mat[index - 1])
-    #     print(rot_theoretical)
-    #     #print(rot_axis_angle_to_ypr(rot_mat))
-    #     #print(error_ypr(rot_mat[index], rot_theoretical))
-    #     print(rot_axis_angle_to_ypr(-40 - 20, rot_mat[0][:, 1], rot_mat[0]))
-
-def test():
-    filenames = ['quaternions-2020-12-16--13-27-22.q',
-                 'quaternions-2020-12-16--13-33-17.q',
-                 'quaternions-2020-12-16--13-43-10.q',
-                 'quaternions-2020-12-16--17-31-17.q']
-
-    index = 3
-    list_quaternion = load_quaternions(filename=filenames[index])
-    #print_list(list_quaternion)
-    #print()
-    initial_q = list_quaternion[0]
-    list_q = list_quaternion
-    yaws = calibrate_angle0_world(initial_q, list_q)
-    #print_yaws(initial_q, yaws)
-
-    yaws = calibrate_angle0_world(initial_q, list_q)
-    yaws_0 = calibrate_angle0_world((1, 0, 0, 1), list_q)
-    for index in range(len(yaws)):
-        y = yaws[index]
-        y0 = yaws_0[index]
-        print(f"{y} --> {y0}")
-
-    list_quaternion = load_quaternions('quaternions-2020-12-16--19-32-02.q')
-    #list_quaternion = load_quaternions('quaternions-2020-12-16--20-07-16.q')
-    q0, q1, q2 = list_quaternion[0], list_quaternion[1], list_quaternion[2]
-    Rq0, Rq1, Rq2 = quat_to_rotmat_world(q0), quat_to_rotmat_world(q1), quat_to_rotmat_world(q2)
-
-    # giro en z de q1
-    yawsRq2Thz = rot_axis_angle_to_ypr(-20, [0, 0, 1], Rq1)
-    print(error_ypr(Rq2, yawsRq2Thz))
-
-    # giro en y
-    yawsRq1Thy = rot_axis_angle_to_ypr(180 - 40, Rq0[:, 1], Rq0)
-    print(error_ypr(Rq1, yawsRq1Thy)) #1
-
-    yawsRq2Thy = rot_axis_angle_to_ypr(-40 - 20, Rq0[:, 1], Rq0)
-    print(error_ypr(Rq2, yawsRq2Thy)) #1
-
-
-###########
-# # yawsRq0=rotation_matrix_to_ypr(Rq0)
-# # yawsRq1=rotation_matrix_to_ypr(Rq1)
-# # yawsRq2=rotation_matrix_to_ypr(Rq2)
-#
-# # giro en z de q1
-# yawsRq2Thz = rot_theoretical_ypr(-20, [0, 0, 1], Rq1)
-#
-# #print(yawsRq2Thz, y1)
-#
-# print(error_ypr(Rq2, yawsRq2Thz))
-#
-# # print(abs(yawsRq2[0]-yawsRq2Thz[0])) # error en yaw
-# # print(abs(yawsRq2[1]-yawsRq2Thz[1])) # error en picth
-#
-# # giro en y
-# yawsRq1Thy = rot_theoretical_ypr(180 - 40, Rq0[:, 1], Rq0)
-# print(error_ypr(Rq1, yawsRq1Thy)) #1
-# #print(abs(yawsRq1[1]- yawsRq1Thy[1]))
-# yawsRq2Thy = rot_theoretical_ypr(-40 - 20, Rq0[:, 1], Rq0)
-# print(error_ypr(Rq2, yawsRq2Thy)) #1
-# #print(abs(yawsRq2[1]- yawsRq2Thy[1]))
