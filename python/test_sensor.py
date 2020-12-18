73e321a5173ef21b2e54a43a90656c1da687fb2a
esmitt <esmitt.ramirez@gmail.com>
2020-12-18 15:43:46 +0100

    new functions added by Debora

--- python/test_sensor.py
+++ python/test_sensor.py
@@ -1,4 +1,21 @@
+"""
+Exp1: Yaw validation
+     Compare R_[0 0 1](yaw_i) * Rq0 with Rq_i
+Theoretical Motion-Yaw: rot axis is g=[0,0,1]
+by experimental design sphere is on ground
+Exp2: Pitch validation
+     Compare R_[Rq0_y](picth_i) * Rq0 with Rq_i
+Theoretical Motion-Pitch: rot axis is y-axis of intial quaternion (noted Rq0_y).
+We assume that it is aligned with sphere
+Exp3: Roll validation (sensor is moved along its y axis picth_i degrees)
+     Compare R_[Rq0_x](picth_i) * Rq0 with Rq_i
+Theoretical Motion-Roll: rot axis is x-axis of intial quaternion (noted Rq0_x).
+We assume that it is aligned with sphere
+"""
+
 from sensor import *
+import sys
+import os
 import pickle
 
 def load_quaternions(filename: str = "quaternions.q") -> list:
@@ -16,6 +33,10 @@ def print_yaws(initial_q, ylist: list):
     print(f"initial quaternion:\n{initial_q}")
     for index in range(len(ylist)):
         print(f"{index+1}: {ylist[index]}")
+
+def get_theoretical_rotmat(angle: float, vth, rotmat0):
+    theoretical_rot = get_rotmat(radians(angle), vth[0],vth[1],vth[2])
+    return (np.matmul(theoretical_rot,rotmat0))
 
 def rot_axis_angle_to_ypr(degrees: float, v: list, source: np.ndarray) -> tuple:
     """
@@ -36,21 +57,87 @@ def error_ypr(rotmat: np.ndarray, ypr_theoretical: tuple) -> tuple:
     :return:
     """
     ypr = rotation_matrix_to_ypr(rotmat)
+    error = np.abs(np.subtract(ypr, ypr_theoretical))
+    return error
+
+def error_in_rotsensor_ypr(rotmat: np.ndarray, rotmat_theoretical: np.ndarray) -> tuple:
+    """
+    Function to return error between theoretical and sensor rotations:
+        rotmat_theoretical*rotmat0=rotmat
+    :param rotmat: rotation given by sensor.
+    :param rotmat0: sensor initial rotation matrix
+    :param rotmat_theoretical: theoretical rotation matrix
+    :return:
+    """
+    ypr = rotation_matrix_to_ypr(rotmat)
+    ypr_theoretical = rotation_matrix_to_ypr(rotmat_theoretical)  # mew added
     error = np.abs(np.subtract(ypr , ypr_theoretical))
     return error
 
 if __name__ == '__main__':
-    filenames = ['test_Dec_2020/quaternions-2020-12-17--11-22-16.q',
-                 'test_Dec_2020/quaternions-2020-12-17--11-27-33.q']
-    index = 1
-    quaternions = load_quaternions(filename=filenames[index])
+    #code_main_dir = r'J:\Experiments\Endoscopy\Navigation\Sensor\Code\python'
+    code_main_dir = os.getcwd()
+
+    sys.path.append(code_main_dir)
+
+    #result_dir = r'J:\Experiments\Endoscopy\Navigation\Sensor\Results\test_Dec_2020'
+    result_dir = os.path.join(code_main_dir, 'test_Dec_2020')
+
+
+    filenames = ['quaternions-2020-12-17--11-22-16.q',
+                 'quaternions-2020-12-17--11-27-33.q']
+
+    # Pitch Validation
+    index = 0
+    filename = os.path.join(result_dir, filenames[index])
+    quaternions = load_quaternions(filename=filename)
     rot_mat = [quat_to_rotmat_world(q) for q in quaternions]
-    angles = [0, 20, 40, 60 , 80]
+    sensor_yaws = [rotation_matrix_to_ypr(rot) for rot in rot_mat]
 
-    i = 1
-    theoretical_rot = rot_axis_angle_to_ypr(angles[i], [1, 0, 0], rot_mat[i])
-    print(error_ypr(rot_mat[i], theoretical_rot))
+    angles = [0, 20, 40, 60, 80]
+    angles = [0, -20, -40, -60, -80]
+    angles_pitch = [0, -20, -40, -60, -80]
+    angles_pitch_yaw = [0, -10, -20, -30, -40]
 
-    i = 2
-    theoretical_rot = rot_axis_angle_to_ypr(angles[i], rot_mat[0][:, 1], rot_mat[i])
-    print(error_ypr(rot_mat[i], theoretical_rot))
+    # Tenemos un problema de orientacion: Necesitamos saber dnd monto el agujerito
+
+    # Theoretical Motion-Roll: rot axis is x-axis of intial quaternion
+    rotmat0 = rot_mat[0]
+    theoretical_rot_roll = [get_theoretical_rotmat(angle, rotmat0[:, 0], rotmat0) for angle in angles]
+    theoretical_yaws_roll = [rotation_matrix_to_ypr(theoretical_rot) for theoretical_rot in theoretical_rot_roll]
+
+    # Exp3b: Theoretical Motion-Roll-Yaw:
+    rotmat0 = rot_mat[1]
+    theoretical_rot_roll_yaw = [get_theoretical_rotmat(angle, [0, 0, 1], rotmat0) for angle in angles_pitch_yaw]
+    theoretical_yaws_roll_yaw = [rotation_matrix_to_ypr(theoretical_rot) for theoretical_rot in
+                                 theoretical_rot_roll_yaw]
+
+    # Exp2: Theoretical Motion-Pitch: rot axis is y-axis of intial quaternion.
+    # We assume that it is aligned with sphere
+    rotmat0 = rot_mat[0]
+    theoretical_rot_pitch = [get_theoretical_rotmat(angle, rotmat0[:, 1], rotmat0) for angle in angles]
+    theoretical_yaws_pitch = [rotation_matrix_to_ypr(theoretical_rot) for theoretical_rot in theoretical_rot_pitch]
+
+    # Exp2b: Theoretical Motion-Pitch-Yaw:
+    rotmat0 = rot_mat[1]
+    theoretical_rot_pitch_yaw = [get_theoretical_rotmat(angle, [0, 0, 1], rotmat0) for angle in angles_pitch_yaw]
+    theoretical_yaws_pitch_yaw = [rotation_matrix_to_ypr(theoretical_rot) for theoretical_rot in
+                                  theoretical_rot_pitch_yaw]
+
+    # Exp1: Theoretical Motion-Yaw: rot axis is g=[0,0,1] by experimental design (sphere is on ground)
+    #
+    g = [0, 0, 1]
+    rotmat0 = rot_mat[0]
+    theoretical_rot_yaw = [get_theoretical_rotmat(angle, g, rotmat0) for angle in angles]
+    theoretical_yaws_yaw = [rotation_matrix_to_ypr(theoretical_rot) for theoretical_rot in theoretical_rot_yaw]
+
+    # print(theoretical_yaws)
+    # print(sensor_yaws)
+
+    #print(f"yaw:{theoretical_yaws[0]}, picth:{theoretical_yaws[1]}, roll:{theoretical_yaws[2]}")
+    print(f"yaw:{sensor_yaws[0]}, picth:{sensor_yaws[1]}, roll:{sensor_yaws[2]}")
+
+    index = 0  # Error in Pitch
+    err = abs(np.array(theoretical_yaws_pitch) - np.array(sensor_yaws))
+    err = np.array(err)
+    print(f" mean:{np.mean(err[:, 1])} ,std:{np.std(err[:, 1])}")
