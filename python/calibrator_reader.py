2a55363f73f93b27fd783632d787e3abddcf3d90
esmitt <esmitt.ramirez@gmail.com>
2021-02-08 17:30:35 +0100

    new version (v6) of the Arduino code including changes on the reader code

--- python/calibrator_reader.py
+++ python/calibrator_reader.py
@@ -10,6 +10,7 @@ from pygame.locals import *
 from datetime import datetime
 import sensor
 import pickle
+import struct
 
 # values to be stored in file
 list_values = list()
@@ -48,6 +49,7 @@ def main():
         ax0, ay0, az0 = None, None, None
         theta, phi = 0, 0
         list_q = None
+        initial_q = None
 
         while True:
             event = pygame.event.poll()  # get a single event from the queue
@@ -75,7 +77,7 @@ def main():
                     #current_frame = 0
                     #origin_axis = Nnone
                     list_origin_axis.clear()
-                    initial_q = (w, nx, ny, nz)
+                    initial_q = [w, nx, ny, nz]
                     print(initial_q)
                     ax0, ay0, az0 = sensor.compute_axis(w, nx, ny, nz)
                     list_origin_axis.append(ax0)
@@ -135,7 +137,7 @@ def main():
                 break
             # endregion
 
-            [w, nx, ny, nz, gravity_x, gravity_y, gravity_z, sys_cal, accel_cal, gyro_cal, mag_cal] = read_data(ser)
+            [w, nx, ny, nz, yaw, pitch, roll, gravity_x, gravity_y, gravity_z, sys_cal, accel_cal, gyro_cal, mag_cal] = read_data(ser)
 
             if flag_initial:
                 list_quaternion.append([w, nx, ny, nz])
@@ -177,13 +179,13 @@ def main():
             #         list_axis.append(c)
             #         #print(w, nx, ny, nz)
 
-            list_axis = list()
-            #rx, ry, rz = sensor.compute_sensor_axis2(w, nx, ny, nz)
-            ax, ay, az = sensor.compute_axis(w, nx, ny, nz)
-            list_axis.append(ax)
-            list_axis.append(ay)
-            list_axis.append(az)
-            draw(w, nx, ny, nz, [sys_cal, accel_cal, gyro_cal, mag_cal], list_axis, list_origin_axis, (theta, phi))
+            # list_axis = list()
+            # #rx, ry, rz = sensor.compute_sensor_axis2(w, nx, ny, nz)
+            # ax, ay, az = sensor.compute_axis(w, nx, ny, nz)
+            # list_axis.append(ax)
+            # list_axis.append(ay)
+            # list_axis.append(az)
+            draw(w, nx, ny, nz, [yaw, pitch, roll], [gravity_x, gravity_y, gravity_z], [sys_cal, accel_cal, gyro_cal, mag_cal], initial_q, list_q, list_origin_axis, (theta, phi))
 
             pygame.display.flip()
             frames += 1
@@ -221,43 +223,52 @@ def init() -> ():
 
 
 def read_data(ser: serial) -> list:
-    PAYLOAD_SIZE = 32
-    ser.flushInput()  # Flush input buffer, discarding all it’s contents.
-    while not(ser.read() == b'B' and ser.read() == b'F'):
-        pass        
-                
-    while ser.in_waiting < PAYLOAD_SIZE:
+    ser.reset_input_buffer()  # Flush input buffer, discarding all it’s contents.
+
+    while not (ser.read() == b'B' and ser.read() == b'F'):
         pass
-        
-    payload = ser.read(PAYLOAD_SIZE)
-    
-    # quaternion data
+
+    while ser.in_waiting < 44:
+        pass
+
+    payload = ser.read(44)
+
+    # quaternion
     quat = struct.unpack('4f', payload[0:16])
     w = quat[0]
     nx = quat[1]
     ny = quat[2]
     nz = quat[3]
-    # gravity vector - m/s^2
-    gravity = struct.unpack('3f', payload[16:28])
+
+    # Euler angles - degrees
+    euler_ang = struct.unpack('3f', payload[16:28])
+    yaw = euler_ang[0]
+    pitch = euler_ang[1]
+    roll = euler_ang[2]
+
+    # gravity vector  - m/s^2
+    gravity = struct.unpack('3f', payload[28:40])
     gravity_x = gravity[0]
     gravity_y = gravity[1]
     gravity_z = gravity[2]
 
     # calibration status
-    sys_cal = payload[28]
-    accel_cal = payload[29]
-    gyro_cal = payload[30]
-    mag_cal = payload[31]
+    sys_cal = payload[40]
+    accel_cal = payload[41]
+    gyro_cal = payload[42]
+    mag_cal = payload[43]
 
-    # print(f"{w:10.6f}, {nx:10.6f}, {ny:10.6f}, {nz:10.6f}")
-    # print("{:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:4d}, {:4d}, {:4d}, {:4d}".
-    #       format(w, nx, ny, nz, gravity_x, gravity_y, gravity_z, sys_cal, accel_cal, gyro_cal, mag_cal))
+    # print("{:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:10.6f}, {:4d}, {:4d}, {:4d}, {:4d}".format(w, nx, ny, nz,
+    #                                                                                                                                              yaw, pitch, roll,
+    #                                                                                                                                              gravity_x, gravity_y, gravity_z,
+    #                                                                                                                                              sys_cal, accel_cal, gyro_cal, mag_cal))
 
-    return [w, nx, ny, nz, gravity_x, gravity_y, gravity_z, sys_cal, accel_cal, gyro_cal, mag_cal]
+    return [w, nx, ny, nz, yaw, pitch, roll, gravity_x, gravity_y, gravity_z, sys_cal, accel_cal, gyro_cal, mag_cal]
 
 
 #def draw(w, nx, ny, nz: float, calibrator: list, list_axis: list, current_axis, last_axis: np.ndarray = None) -> ():
-def draw(w, nx, ny, nz: float, calibrator: list, list_axis, list_origin_axis: list, angles: tuple) -> ():
+def draw(w, nx, ny, nz: float, ypr: list, gravity: list, calibrator: list, initial_q: tuple, list_q:list,
+         list_origin_axis: list, angles: tuple) -> ():
     # sys_cal, accel_cal, gyro_cal, mag_cal
     def draw_sensor() -> ():
         glBegin(GL_QUADS)
@@ -305,6 +316,7 @@ def draw(w, nx, ny, nz: float, calibrator: list, list_axis, list_origin_axis: li
 
     startXRight = -1.0
     startYRight = +.90
+
     # Header and footer
     draw_text((startXRight, startYRight, -2.5), "MARG sensor: BNO055 | Sensor controller (Arduino MEGA)", 16, (150, 100, 240, 255))
     draw_text((startXRight, startYRight - 0.1, -2.5), "Press Escape to exit.", 14, (220, 100, 10, 255))
@@ -314,10 +326,26 @@ def draw(w, nx, ny, nz: float, calibrator: list, list_axis, list_origin_axis: li
     draw_text((startXRight + 1.0, startYRight - 0.2, -2.5), "Press 'p' to record quaternion", 14, (220, 100, 10, 255))
     draw_text((startXRight, startYRight - 0.3, -2.5), "Press 'i' for initial quaternion", 14, (220, 100, 10, 255))
 
-    # Sensor calibration status
-    draw_text((-2.6, 1.5, 2), "Sensor Calibration Status", 16)
-    draw_text((-2.6, 1.3, 2), f"Syst:{calibrator[0]}, Accel:{calibrator[1]}, Gyro:{calibrator[2]}, Mag:{calibrator[3]}",
-              16)
+    # Sensor values received directly
+    draw_text((startXRight, startYRight - 0.4, -2.5), "Sensor Calibration Status", 12, (120, 225, 10, 255))
+    draw_text((startXRight + 1.0, startYRight - 0.4, -2.5), f"Syst:{calibrator[0]}, Accel:{calibrator[1]}, Gyro:{calibrator[2]}, Mag:{calibrator[3]}",
+              12, (120, 225, 10, 255))
+    draw_text((startXRight, startYRight - 0.45, -2.5), "Sensor Gravity Values", 12, (120, 225, 10, 255))
+    draw_text((startXRight + 1.0, startYRight - 0.45, -2.5), f"X:{gravity[0]:.4f}, Y:{gravity[1]:.4f}, "
+                                                             f"Z:{gravity[2]:.4f}", 12, (120, 225, 10, 255))
+
+    draw_text((startXRight, startYRight - 0.5, -2.5), "Sensor Gravity Values", 12, (120, 225, 10, 255))
+    draw_text((startXRight + 1.0, startYRight - 0.5, -2.5), f"X:{gravity[0]:.4f}, Y:{gravity[1]:.4f}, "
+                                                             f"Z:{gravity[2]:.4f}", 12, (120, 225, 10, 255))
+
+    draw_text((startXRight, startYRight - 0.5, -2.5), "Sensor Yaw-Pitch-Roll Values", 12, (120, 225, 10, 255))
+    draw_text((startXRight + 1.0, startYRight - 0.5, -2.5), f"Yaw:{ypr[0]:.4f}, Pitch:{ypr[1]:.4f}, "
+                                                            f"Roll:{ypr[2]:.4f}", 12, (120, 225, 10, 255))
+
+    draw_text((startXRight, startYRight - 0.55, -2.5), "Sensor Quaternion Values", 12, (120, 225, 10, 255))
+    draw_text((startXRight + 1.0, startYRight - 0.55, -2.5), f"({w:.4f}, {nx:.4f}, {ny:.4f}, {nz:.4f})", 12,
+              (120, 225, 10, 255))
+
     # Sensor data
     # if sensor_axis is None:
     #     draw_text((-2.6, -1., 2), " Waiting to set initial position", 16)
@@ -327,7 +355,7 @@ def draw(w, nx, ny, nz: float, calibrator: list, list_axis, list_origin_axis: li
     #     draw_text((-2.6, -1., 2), f"origin axis: {sensor_axis[0]:.3f}, {sensor_axis[1]:.3f}, {sensor_axis[2]:.3f}", 16,
     #               (180, 190, 140, 255))
 
-    startYRight = -0.5
+    startYRight = -0.45
 
     # quaternion
     norm = math.sqrt(nx * nx + ny * ny + nz * nz)
@@ -337,57 +365,113 @@ def draw(w, nx, ny, nz: float, calibrator: list, list_axis, list_origin_axis: li
     uy = ny / norm
     uz = nz / norm
     angle = 2.0 * math.acos(w) * 180.00 / math.pi
-    draw_text((startXRight, startYRight, -2.5), f"Q normalized: {angle:.3f}, {ux:.3f}, {uy:.3f} {uz:.3f}", 16)
+    draw_text((startXRight, startYRight, -2.5), f"Q normalized: ({angle:.3f}, {ux:.3f}, {uy:.3f} {uz:.3f})", 14)
 
     # # axis
     # draw_text((startXRight, startYRight - 0.1, -2.5),
     #           f"rx: [{R[:, 0]:.2f}, {R[:, 1]:.2f}, {R[:,2]:.2f}]", 16,
     #           color=(199, 199, 39, 255))
 
-    if list_axis is not None:
-        #print(list_axis[0])
-        #draw_text((startXRight, startYRight - 0.1, -2.5), f"rx: {list_axis[0]}", 16, color=(199, 199, 39, 255))
-        draw_text((startXRight, startYRight - 0.1, -2.5), f"rx: [{list_axis[0][0]:.2f}, {list_axis[0][1]:.2f}, {list_axis[0][2]:.2f}]", 16, color = (199, 199, 39, 255))
-        draw_text((startXRight, startYRight - 0.2, -2.5), f"ry: [{list_axis[1][0]:.2f}, {list_axis[1][1]:.2f}, {list_axis[1][2]:.2f}]", 16, color = (199, 199, 39, 255))
-        draw_text((startXRight, startYRight - 0.3, -2.5), f"rz: [{list_axis[2][0]:.2f}, {list_axis[2][1]:.2f}, {list_axis[2][2]:.2f}]", 16, color = (199, 199, 39, 255))
+    R = sensor.compute_axis(w, nx, ny, nz)
+    #draw_text((startXRight, startYRight - 0.1, -2.5), f"rx: {list_axis[0]}", 16, color=(199, 199, 39, 255))
+    draw_text((startXRight, startYRight - 0.05, -2.5), "Compute_axis:", 14, color=(150, 250, 99, 255))
+    draw_text((startXRight, startYRight - 0.10, -2.5), f"rx: [{R[0][0]:.2f}, {R[0][1]:.2f}, {R[0][2]:.2f}]", 14, color = (230, 230, 39, 255))
+    draw_text((startXRight, startYRight - 0.15, -2.5), f"ry: [{R[1][0]:.2f}, {R[1][1]:.2f}, {R[1][2]:.2f}]", 14, color = (230, 230, 39, 255))
+    draw_text((startXRight, startYRight - 0.20, -2.5), f"rz: [{R[2][0]:.2f}, {R[2][1]:.2f}, {R[2][2]:.2f}]", 14, color = (230, 230, 39, 255))
+    yaw, pitch, roll = sensor.rotation_matrix_to_ypr(np.array(R))
+    draw_text((startXRight, startYRight - 0.25, -2.5), f"{{{yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+              color=(230, 130, 99, 255))
 
-    if len(list_origin_axis) == 3:
-        draw_text((startXRight, startYRight - 0.4, -2.5),
-                  f"rx: [{list_origin_axis[0][0]:.2f}, {list_origin_axis[0][1]:.2f}, {list_origin_axis[0][2]:.2f}]", 14,
-                  color=(199, 39, 199, 255))
-        draw_text((startXRight, startYRight - 0.45, -2.5),
-                  f"ry: [{list_origin_axis[1][0]:.2f}, {list_origin_axis[1][1]:.2f}, {list_origin_axis[1][2]:.2f}]", 14,
-                  color=(199, 39, 199, 255))
-        draw_text((startXRight, startYRight - 0.50, -2.5),
-                  f"rz: [{list_origin_axis[2][0]:.2f}, {list_origin_axis[2][1]:.2f}, {list_origin_axis[2][2]:.2f}]", 14,
-                  color=(199, 39, 199, 255))
+    # other techniques
+    draw_text((startXRight + 1, startYRight - 0.05, -2.5), "get_rotmat:", 14, color=(150, 250, 99, 255))
+    R = sensor.get_rotmat(w, nx, ny, nz)
+    draw_text((startXRight + 1, startYRight - 0.10, -2.5),
+              f"rx: [{R[0][0]:.2f}, {R[0][1]:.2f}, {R[0][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    draw_text((startXRight + 1, startYRight - 0.15, -2.5),
+              f"ry: [{R[1][0]:.2f}, {R[1][1]:.2f}, {R[1][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    draw_text((startXRight + 1, startYRight - 0.20, -2.5),
+              f"rz: [{R[2][0]:.2f}, {R[2][1]:.2f}, {R[2][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    yaw, pitch, roll = sensor.rotation_matrix_to_ypr(R)
+    draw_text((startXRight + 1, startYRight - 0.25, -2.5), f"{{{yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+              color=(230, 130, 99, 255))
 
-        # angles
-        Tax, Tay, Taz = sensor.compute_axis_theoretical(angles[0], angles[1], list_origin_axis[0], list_origin_axis[1], list_origin_axis[2])
+    startYRight -= 0.25
+    draw_text((startXRight, startYRight - 0.05, -2.5), "quat_to_rotmat_world:", 14, color=(150, 250, 99, 255))
+    R = sensor.quat_to_rotmat_world([w, nx, ny, nz])
+    draw_text((startXRight, startYRight - 0.10, -2.5),
+              f"rx: [{R[0][0]:.2f}, {R[0][1]:.2f}, {R[0][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    draw_text((startXRight, startYRight - 0.15, -2.5),
+              f"ry: [{R[1][0]:.2f}, {R[1][1]:.2f}, {R[1][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    draw_text((startXRight, startYRight - 0.20, -2.5),
+              f"rz: [{R[2][0]:.2f}, {R[2][1]:.2f}, {R[2][2]:.2f}]", 14,
+              color=(230, 230, 39, 255))
+    yaw, pitch, roll = sensor.rotation_matrix_to_ypr(R)
+    draw_text((startXRight, startYRight - 0.25, -2.5), f"{{{yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+              color=(230, 130, 99, 255))
 
-        startXRight += 0.75
-        draw_text((startXRight, startYRight - 0.35, -2.5),f"theta: {angles[0]}, phi: {angles[1]}", 14, color=(155, 155, 155, 255))
-        draw_text((startXRight, startYRight - 0.4, -2.5),
-                  f"Tax: [{Tax[0]:.2f}, {Tax[1]:.2f}, {Tax[2]:.2f}]", 14, color=(39, 199, 199, 255))
-        draw_text((startXRight, startYRight - 0.45, -2.5),
-                  f"Tay: [{Tay[0]:.2f}, {Tay[1]:.2f}, {Tay[2]:.2f}]", 14, color=(39, 199, 199, 255))
-        draw_text((startXRight, startYRight - 0.50, -2.5),
-                  f"Taz: [{Taz[0]:.2f}, {Taz[1]:.2f}, {Taz[2]:.2f}]", 14, color=(39, 199, 199, 255))
-    # # if isinstance(current_axis, str):
-    #     draw_text((-2.6, -1.2, 2), f"Calculating current position {current_axis}...wait", 16)
-    # elif isinstance(current_axis, np.ndarray):
-    #     draw_text((-2.6, -1.2, 2), f"current axis: {current_axis[0]:.3f}, {current_axis[1]:.3f}, "
-    #                                f"{current_axis[2]:.3f}", 16)
+    startXRight += 1.0
+    if initial_q is not None:
+        # R = sensor.quat_to_rotmat_world([w, nx, ny, nz])
+        # yaw, pitch, roll = sensor.rotation_matrix_to_ypr(R)
+        # draw_text((startXRight, startYRight -0.05, -2.5), f"0){yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+        #           color=(240, 130, 199, 255))
+        R = sensor.quat_to_rotmat_world([initial_q[0], initial_q[1], initial_q[2], initial_q[3]])
+        yaw, pitch, roll = sensor.rotation_matrix_to_ypr(R)
+        draw_text((startXRight, startYRight - 0.05, -2.5), f"0){yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+                  color=(240, 130, 199, 255))
+        startYRight -= 0.05
+
+        if list_q is not None:
+            for index in range(1, len(list_q)):
+                startYRight -= 0.05
+                R = sensor.quat_to_rotmat_world([list_q[index][0], list_q[index][1], list_q[index][2], list_q[index][3]])
+                yaw, pitch, roll = sensor.rotation_matrix_to_ypr(R)
+                draw_text((startXRight, startYRight, -2.5), f"{index}){yaw:.3f}, {pitch:.3f}, {roll:.3f}}}", 14,
+                          color=(240, 130, 199, 255))
+# if len(list_origin_axis) == 3:
+#     draw_text((startXRight, startYRight - 0.50, -2.5),
+#               f"rx: [{list_origin_axis[0][0]:.2f}, {list_origin_axis[0][1]:.2f}, {list_origin_axis[0][2]:.2f}]", 14,
+#               color=(199, 39, 199, 255))
+#     draw_text((startXRight, startYRight - 0.55, -2.5),
+#               f"ry: [{list_origin_axis[1][0]:.2f}, {list_origin_axis[1][1]:.2f}, {list_origin_axis[1][2]:.2f}]", 14,
+#               color=(199, 39, 199, 255))
+#     draw_text((startXRight, startYRight - 0.60, -2.5),
+#               f"rz: [{list_origin_axis[2][0]:.2f}, {list_origin_axis[2][1]:.2f}, {list_origin_axis[2][2]:.2f}]", 14,
+#               color=(199, 39, 199, 255))
+
+    # angles
+    # Tax, Tay, Taz = sensor.compute_axis_theoretical(angles[0], angles[1], list_origin_axis[0], list_origin_axis[1], list_origin_axis[2])
     #
-    #
-    #
-    #
-    # if isinstance(last_axis, np.ndarray):
-    #     draw_text((-2.8, -1.2, 2), f"last axis: {last_axis[0]:.3f}, {last_axis[1]:.3f}, {last_axis[2]:.3f}", 16,
-    #               (180, 190, 140, 255))
-    # draw more in Y -1.6 or -1.8
+    # startXRight += 0.75
+    # draw_text((startXRight, startYRight - 0.35, -2.5),f"theta: {angles[0]}, phi: {angles[1]}", 14, color=(155, 155, 155, 255))
+    # draw_text((startXRight, startYRight - 0.4, -2.5),
+    #           f"Tax: [{Tax[0]:.2f}, {Tax[1]:.2f}, {Tax[2]:.2f}]", 14, color=(39, 199, 199, 255))
+    # draw_text((startXRight, startYRight - 0.45, -2.5),
+    #           f"Tay: [{Tay[0]:.2f}, {Tay[1]:.2f}, {Tay[2]:.2f}]", 14, color=(39, 199, 199, 255))
+    # draw_text((startXRight, startYRight - 0.50, -2.5),
+    #           f"Taz: [{Taz[0]:.2f}, {Taz[1]:.2f}, {Taz[2]:.2f}]", 14, color=(39, 199, 199, 255))
+
+
+# # if isinstance(current_axis, str):
+#     draw_text((-2.6, -1.2, 2), f"Calculating current position {current_axis}...wait", 16)
+# elif isinstance(current_axis, np.ndarray):
+#     draw_text((-2.6, -1.2, 2), f"current axis: {current_axis[0]:.3f}, {current_axis[1]:.3f}, "
+#                                f"{current_axis[2]:.3f}", 16)
+#
+#
+#
+#
+# if isinstance(last_axis, np.ndarray):
+#     draw_text((-2.8, -1.2, 2), f"last axis: {last_axis[0]:.3f}, {last_axis[1]:.3f}, {last_axis[2]:.3f}", 16,
+#               (180, 190, 140, 255))
+# draw more in Y -1.6 or -1.8
     glPushMatrix()
-    glTranslatef(0, 0.5, -8)
+    glTranslatef(0, -0.1, -10)
     glRotatef(2 * math.acos(w) * 180.00 / math.pi, -nx, nz, ny)
     glScalef(1, 4, 1)
     draw_sensor()
@@ -413,3 +497,4 @@ if __name__ == '__main__':
     # axis = [0.2, 0.1, 0.7]
     # the = compute_sensor_theoretical(0, 0, axis)
     # print(the)
+
